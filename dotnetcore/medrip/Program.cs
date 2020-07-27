a6a2f343093d79bc213baca36f7e5f167e81e2b9
Simon Nishi McCorkindale <simon@funx.jp>
2020-07-28 08:52:22 +0900

    Initial C# implementation of Mind Enabled Driver entropy Pusher to Azure Storage Queue

---
+++ dotnetcore/medrip/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace medrip
+{
+    class Program
+    {
+        [DllImport("qwqng", CallingConvention = CallingConvention.Cdecl)]
+        public extern static void randbytes(byte[] buffer, int bytecount);
+
+        static void Main(string[] args)
+        {
+            var s = "DefaultEndpointsProtocol=https;AccountName=medentropyqueue;AccountKey=YsLKNOquLXl/f5dMS6aL0RjVIC0BrOvuOsKsc5FgbNVIyi9GCKF9Wjzl+SoNgd8sPiBKN9ZfnGmbf9W52f+4Nw==;EndpointSuffix=core.windows.net";
+            var n = "entropy-queue";
+            var p = new Pusher(s, n);
+
+            var buffer = new List<byte>();
+            var slen = 8192;
+            while (true)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    var smallBuff = new byte[slen];
+                    randbytes(smallBuff, slen);
+                    buffer.AddRange(smallBuff);
+                }
+                p.SendEntropy(buffer);
+                buffer.Clear();
+            }
+        }
+    }
+}
---
+++ dotnetcore/medrip/Pusher.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Azure.Storage.Queues;
+
+namespace medrip
+{
+    public class Pusher
+    {
+        string connectionString;
+        string queueName;
+        QueueClient queue;
+
+        public Pusher(string connectionString, string queueName)
+        {
+            this.connectionString = connectionString;
+            this.queueName = queueName;
+            queue = new QueueClient(connectionString, queueName);
+            queue.Create();
+        }
+
+        public void SendEntropy(List<byte> entropyBinary)
+        {
+            string entropStr = Convert.ToBase64String(entropyBinary.ToArray());
+            entropStr = $"<entropy>{entropStr}</entropy>";
+            var ttl = new TimeSpan(3, 0, 0); // hrs|min|sec
+            queue.SendMessage(entropStr, timeToLive: ttl);
+            Console.WriteLine($"{DateTime.Now.ToString()} sent {entropStr.Length} bytes");
+        }
+    }
+}
